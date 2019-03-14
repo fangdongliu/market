7f954da6288d6a2e10859ce9eb5db880c07eef5c
Shannon2016 <shannonsn@qq.com>
2019-03-14 10:35:53 +0800

    2019/3/14 10:35 Shannon-备案

---
+++ src/main/resources/static/js/jump.js
@@ -0,0 +1,11 @@
+function jumpProvinceRecord() {
+    document.getElementById('menuFrame').src = "provinceRecord.html"
+}
+
+function jumpPointRecord() {
+    document.getElementById('menuFrame').src = "pointRecord.html"
+}
+
+function jumpPointRecord() {
+    document.getElementById('menuFrame').src = "marketDetail.html"
+}
