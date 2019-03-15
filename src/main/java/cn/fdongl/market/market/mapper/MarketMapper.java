d92fa61c777fc0793c12970fcad9bd0a10cb3773
[BIT-zhangxin] <1047632321@qq.com>
2019-03-15 21:48:06 +0800

    备案接口完善2（内容还未完成）
    from 张歆
    再次修改

--- src/main/java/cn/fdongl/market/market/mapper/MarketMapper.java
+++ src/main/java/cn/fdongl/market/market/mapper/MarketMapper.java
@@ -10,37 +10,30 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface MarketMapper {
 
-    //用户首次保存，新建一条备案信息
+    //用户新建一条备案信息
     @Insert("INSERT INTO" +
-            "t_record_info(region_emp_id,region_emp_name,region_name,region_emp_contact,region_emp_contact_mobi,region_emp_contact_num,region_emp_fax,state_flag,apply_date,examine_date)" +
-            "VALUES(#{param1},#{regionEmpName},#{regionName},#{regionEmpContact},#{regionEmpContactMobi},#{regionEmpContactNum},#{regionEmpFax},#{stateFlag},#{applyDate},#{examineDate})")
-    Integer insert(Integer userId,Record record,Integer stateFlag);
+            "t_record_info(region_emp_id,region_emp_name,region_name,region_emp_contact,region_emp_contact_mobi,region_emp_contact_num,region_emp_fax,state_flag,create_time,creator,revise_time,reviser)" +
+            "VALUES(#{regionEmpId},#{regionEmpName},#{regionName},#{regionEmpContact},#{regionEmpContactMobi},#{regionEmpContactNum},#{regionEmpFax},#{stateFlag},#{createTime},#{creator},#{reviseTime},#{reviser})")
+    Integer insert(Record record);
 
-    //用户修改保存信息,或者上报备案信息
+    //用户更新一条备案信息
     @Update("UPDATE t_record_info SET" +
-            "region_emp_name=#{regionEmpName}" +
-            "region_name=#{regionName}" +
-            "region_emp_contact=#{regionEmpContact}" +
-            "region_emp_contact_mobi=#{regionEmpContactMobi}" +
-            "region_emp_contact_num=#{regionEmpContactNum}" +
-            "region_emp_fax=#{regionEmpFax}" +
-            "state_flag=#{stateFlag}" +
-            "apply_date=#{applyDate}" +
-            "examine_date=#{examineDate}" +
-            "where region_emp_id=#{param1} and state_flag=0")
-    Integer update(Integer userId,Record record,Integer stateFlag);
+            "region_emp_id=#{regionEmpId}," +
+            "region_emp_name=#{regionEmpName}," +
+            "region_name=#{regionName}," +
+            "region_emp_contact=#{regionEmpContact}," +
+            "region_emp_contact_mobi=#{regionEmpContactMobi}," +
+            "region_emp_contact_num=#{regionEmpContactNum}," +
+            "region_emp_fax=#{regionEmpFax}," +
+            "state_flag=#{stateFlag}," +
+            "create_time=#{createTime}," +
+            "creator=#{creator}," +
+            "revise_time=#{reviseTIme}," +
+            "reviser=#{reviser}" +
+            "where region_emp_id=#{regionEmpId} and state_flag=0;")
+    Integer update(Record record);
 
-    //根据用户id和state查询备案信息
-    @Select("SELECT" +
-            "region_emp_name AS regionEmpName," +
-            "region_name AS regionName," +
-            "region_emp_contact AS regionEmpContact," +
-            "region_emp_contact_mobi AS regionEmpContactMobi," +
-            "region_emp_contact_num AS regionEmpContactNum," +
-            "region_emp_fax AS regionEmpFax," +
-            "state_flag AS stateFlag," +
-            "apply_date AS applyDate," +
-            "examine_date AS examineDate" +
-            "from t_record_info where region_emp_id=#{param1} and state_flag=#{param2}")
+    //根据用户id查询备案信息,存储过程
+    @Select("")
     Record select(Integer userId);
 }
