8dbde22509471aebbabeffe2e798d7adbf94221c
[BIT-zhangxin] <1047632321@qq.com>
2019-03-18 10:00:24 +0800

    修改Record中的creator
    from 张歆
    之前拼错了

--- src/main/java/cn/fdongl/market/market/entity/Record.java
+++ src/main/java/cn/fdongl/market/market/entity/Record.java
@@ -16,7 +16,7 @@ public class Record {
     String regionEmpFax;//联系人传真
     Integer stateFlag;//状态标志，保存0，上传1，通过2，过期3
     Date createTime;//创建时间
-    Integer creater;//创建人id
+    Integer creator;//创建人id
     Date reviseTIme;//审核时间
     Integer reviser;//审核人id
 
