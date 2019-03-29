5605473f512318a5fa4a0b77879255ecd56b1451
LiuFangdong <894856599@qq.com>
2019-03-29 16:48:24 +0800

    刘方东  - swagger 接口文档框架 . ignore AppUserDetail

--- src/main/java/cn/fdongl/market/Swagger2.java
+++ src/main/java/cn/fdongl/market/Swagger2.java
@@ -1,7 +1,10 @@
 package cn.fdongl.market;
 
+import cn.fdongl.market.security.entity.AppUserDetail;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,10 +15,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class Swagger2 {
     @Bean
     public Docket createRestApi(){
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("cn.fdongl.market"))
+        return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(AppUserDetail.class)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.fdongl.market"))
                 .paths(PathSelectors.any()).build();
     }
     private ApiInfo apiInfo(){
