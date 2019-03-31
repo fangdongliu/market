ab37dd9ae79262111698353dc7607282bbbdc56a
LiuFangdong <894856599@qq.com>
2019-03-31 15:11:07 +0800

    刘方东  - 修复 线程安全

--- src/main/java/cn/fdongl/market/util/UniworldExceptionHandler.java
+++ src/main/java/cn/fdongl/market/util/UniworldExceptionHandler.java
@@ -1,19 +1,22 @@
 package cn.fdongl.market.util;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Log4j2
 public class UniworldExceptionHandler {
 
-    Result result = new Result();
+    ThreadLocal<Result> result = ThreadLocal.withInitial(Result::new);
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public Object exception(Exception ex){
-        result.setMessage(ex.getMessage());
-        result.setCode(1);
-        return result;
+        Result rs = result.get();
+        rs.setMessage(ex.getMessage());
+        rs.setCode(1);
+        return rs;
     }
 }
